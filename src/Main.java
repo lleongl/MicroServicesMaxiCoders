d6902bd605b7a3e28095ef7485a0fd325123a7df
JulienMaria <julien.maria@gmail.com>
2023-11-30 10:19:16 +0100

    code to beat bronze league boss

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import java.util.*;
 
 // Define the data structures as records
-record Vector(int x, int y) {}
+record Vector(int x, int y) {
+    static double calculateDistance(Vector point1, Vector point2) {
+        int deltaX = point2.x() - point1.x();
+        int deltaY = point2.y() - point1.y();
+
+        // Applying the Euclidean distance formula: sqrt((x2 - x1)^2 + (y2 - y1)^2)
+        return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+}
 
 record FishDetail(int color, int type) {}
 
@@ -12,111 +20,168 @@ record Drone(int droneId, Vector pos, boolean dead, int battery, List<Integer> s
 record RadarBlip(int fishId, String dir) {}
 
 class Player {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+        public static boolean downLeft = false;
+        public static    boolean downRight = false;
+         public static   boolean downCenter = false;
+        public static    boolean goUp = false;
+        public static int turnNumber = 0 ;
 
-    Map<Integer, FishDetail> fishDetails = new HashMap<>();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-    int fishCount = in.nextInt();
-    for (int i = 0; i < fishCount; i++) {
-      int fishId = in.nextInt();
-      int color = in.nextInt();
-      int type = in.nextInt();
-      fishDetails.put(fishId, new FishDetail(color, type));
+        Map<Integer, FishDetail> fishDetails = new HashMap<>();
+
+        int fishCount = in.nextInt();
+        for (int i = 0; i < fishCount; i++) {
+            int fishId = in.nextInt();
+            int color = in.nextInt();
+            int type = in.nextInt();
+            fishDetails.put(fishId, new FishDetail(color, type));
+        }
+
+        // game loop
+        while (true) {
+            Player.turnNumber += 1;
+            List<Integer> myScans = new ArrayList<>();
+            List<Integer> foeScans = new ArrayList<>();
+            Map<Integer, Drone> droneById = new HashMap<>();
+            List<Drone> myDrones = new ArrayList<>();
+            List<Drone> foeDrones = new ArrayList<>();
+            List<Fish> visibleFishes = new ArrayList<>();
+            Map<Integer, List<RadarBlip>> myRadarBlips = new HashMap<>();
+
+            int myScore = in.nextInt();
+            int foeScore = in.nextInt();
+
+            int myScanCount = in.nextInt();
+            for (int i = 0; i < myScanCount; i++) {
+                int fishId = in.nextInt();
+                myScans.add(fishId);
+            }
+
+            int foeScanCount = in.nextInt();
+            for (int i = 0; i < foeScanCount; i++) {
+                int fishId = in.nextInt();
+                foeScans.add(fishId);
+            }
+
+            
+
+            int myDroneCount = in.nextInt();
+            for (int i = 0; i < myDroneCount; i++) {
+                int droneId = in.nextInt();
+                int droneX = in.nextInt();
+                int droneY = in.nextInt();
+                boolean dead = in.nextInt() == 1;
+                int battery = in.nextInt();
+                Vector pos = new Vector(droneX, droneY);
+                Drone drone = new Drone(droneId, pos, dead, battery, new ArrayList<>());
+                droneById.put(droneId, drone);
+                myDrones.add(drone);
+                myRadarBlips.put(droneId, new ArrayList<>());
+            }
+
+            int foeDroneCount = in.nextInt();
+            for (int i = 0; i < foeDroneCount; i++) {
+                int droneId = in.nextInt();
+                int droneX = in.nextInt();
+                int droneY = in.nextInt();
+                boolean dead = in.nextInt() == 1;
+                int battery = in.nextInt();
+                Vector pos = new Vector(droneX, droneY);
+                Drone drone = new Drone(droneId, pos, dead, battery, new ArrayList<>());
+                droneById.put(droneId, drone);
+                foeDrones.add(drone);
+            }
+
+            
+
+            int droneScanCount = in.nextInt();
+            for (int i = 0; i < droneScanCount; i++) {
+                int droneId = in.nextInt();
+                int fishId = in.nextInt();
+                droneById.get(droneId).scans().add(fishId);
+            }
+
+            int visibleFishCount = in.nextInt();
+            for (int i = 0; i < visibleFishCount; i++) {
+                int fishId = in.nextInt();
+                int fishX = in.nextInt();
+                int fishY = in.nextInt();
+                int fishVx = in.nextInt();
+                int fishVy = in.nextInt();
+                Vector pos = new Vector(fishX, fishY);
+                Vector speed = new Vector(fishVx, fishVy);
+                FishDetail detail = fishDetails.get(fishId);
+                visibleFishes.add(new Fish(fishId, pos, speed, detail));
+            }
+
+            int myRadarBlipCount = in.nextInt();
+            for (int i = 0; i < myRadarBlipCount; i++) {
+                int droneId = in.nextInt();
+                int fishId = in.nextInt();
+                String radar = in.next();
+                myRadarBlips.get(droneId).add(new RadarBlip(fishId, radar));
+            }
+
+            
+
+            String order = "wait 0 no order inputed";
+            boolean putTheLightOn = Player.turnNumber % 3 == 0;
+
+            // System.out.println(String.format("MOVE %d %d %d", targetX, targetY, light));
+            for (Drone drone : myDrones) {
+                int x = drone.pos().x();
+                int y = drone.pos().y();
+                System.err.println(String.format("go up: %b ", Player.goUp));
+                if(Player.goUp == true){
+                    if(Vector.calculateDistance(drone.pos(), new Vector(5000,300)) < 200){                        
+                        Player.goUp = false;                        
+                    }else{
+                        if (putTheLightOn) 
+                            order = new String("MOVE 5000 300 0");
+                        else
+                            order =  new String("MOVE 5000 300 1");
+                    }
+                }
+
+                if(Player.downLeft == false){
+                    if(Vector.calculateDistance(drone.pos(), new Vector(2000,8000)) < 1000){
+                        Player.downLeft = true;
+                        Player.goUp = true;                        
+                    }else{
+                        if (putTheLightOn) 
+                            order = new String("MOVE 2000 9000 0");
+                        else
+                            order =  new String("MOVE 2000 9000 1");
+                    }
+                }
+               
+                if(Player.downCenter == false){
+                    if(Vector.calculateDistance(drone.pos(), new Vector(5000,8000)) < 1000){
+                        Player.downCenter = true;
+                        Player.goUp = true;                        
+                    }else{
+                        if (putTheLightOn) 
+                            order = new String("MOVE 5000 9000 0");
+                        else
+                            order =  new String("MOVE 5000 9000 1");
+                    }
+                }
+                if(Player.downRight == false){
+                    if(Vector.calculateDistance(drone.pos(), new Vector(8000,8000)) < 1000){
+                        Player.downRight = true;
+                        Player.goUp = true;                        
+                    }else{
+                        if (putTheLightOn) 
+                            order = new String("MOVE 8000 9000 0");
+                        else
+                            order =  new String("MOVE 8000 9000 1");
+                    }
+                }
+                System.out.println(order);
+            }
+        }
     }
-
-    // game loop
-    while (true) {
-      List<Integer> myScans = new ArrayList<>();
-      List<Integer> foeScans = new ArrayList<>();
-      Map<Integer, Drone> droneById = new HashMap<>();
-      List<Drone> myDrones = new ArrayList<>();
-      List<Drone> foeDrones = new ArrayList<>();
-      List<Fish> visibleFishes = new ArrayList<>();
-      Map<Integer, List<RadarBlip>> myRadarBlips = new HashMap<>();
-
-      int myScore = in.nextInt();
-      int foeScore = in.nextInt();
-
-      int myScanCount = in.nextInt();
-      for (int i = 0; i < myScanCount; i++) {
-        int fishId = in.nextInt();
-        myScans.add(fishId);
-      }
-
-      int foeScanCount = in.nextInt();
-      for (int i = 0; i < foeScanCount; i++) {
-        int fishId = in.nextInt();
-        foeScans.add(fishId);
-      }
-
-
-
-      int myDroneCount = in.nextInt();
-      for (int i = 0; i < myDroneCount; i++) {
-        int droneId = in.nextInt();
-        int droneX = in.nextInt();
-        int droneY = in.nextInt();
-        boolean dead = in.nextInt() == 1;
-        int battery = in.nextInt();
-        Vector pos = new Vector(droneX, droneY);
-        Drone drone = new Drone(droneId, pos, dead, battery, new ArrayList<>());
-        droneById.put(droneId, drone);
-        myDrones.add(drone);
-        myRadarBlips.put(droneId, new ArrayList<>());
-      }
-
-      int foeDroneCount = in.nextInt();
-      for (int i = 0; i < foeDroneCount; i++) {
-        int droneId = in.nextInt();
-        int droneX = in.nextInt();
-        int droneY = in.nextInt();
-        boolean dead = in.nextInt() == 1;
-        int battery = in.nextInt();
-        Vector pos = new Vector(droneX, droneY);
-        Drone drone = new Drone(droneId, pos, dead, battery, new ArrayList<>());
-        droneById.put(droneId, drone);
-        foeDrones.add(drone);
-      }
-
-      int droneScanCount = in.nextInt();
-      for (int i = 0; i < droneScanCount; i++) {
-        int droneId = in.nextInt();
-        int fishId = in.nextInt();
-        droneById.get(droneId).scans().add(fishId);
-      }
-
-      int visibleFishCount = in.nextInt();
-      for (int i = 0; i < visibleFishCount; i++) {
-        int fishId = in.nextInt();
-        int fishX = in.nextInt();
-        int fishY = in.nextInt();
-        int fishVx = in.nextInt();
-        int fishVy = in.nextInt();
-        Vector pos = new Vector(fishX, fishY);
-        Vector speed = new Vector(fishVx, fishVy);
-        FishDetail detail = fishDetails.get(fishId);
-        visibleFishes.add(new Fish(fishId, pos, speed, detail));
-      }
-
-      int myRadarBlipCount = in.nextInt();
-      for (int i = 0; i < myRadarBlipCount; i++) {
-        int droneId = in.nextInt();
-        int fishId = in.nextInt();
-        String radar = in.next();
-        myRadarBlips.get(droneId).add(new RadarBlip(fishId, radar));
-      }
-
-      for (Drone drone : myDrones) {
-        int x = drone.pos().x();
-        int y = drone.pos().y();
-        // TODO: Implement logic on where to move here
-        int targetX = 5000;
-        int targetY = 5000;
-        int light = 1;
-
-        System.out.println(String.format("MOVE %d %d %d", targetX, targetY, light));
-      }
-    }
-  }
 }
+
